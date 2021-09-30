f73d6168cfbcbfc60be5ae4160844e547ec1138c
Basit Bada <basit.bada@commetix.com>
2021-09-30 07:17:15 -0500

    convert BusinessList.js to tsx

---
+++ src/components/BusinessList/BusinessList.tsx
@@ -0,0 +1,46 @@
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+// type BusinessProps = {
+//     business: {
+//         id: string,
+//         imageSrc: string,
+//         name: string,
+//         address: string,
+//         city: string,
+//         state: string,
+//         zipCode: string,
+//         category: string,
+//         rating: number,
+//         reviewCount: number,
+//     },
+// };
+
+type BusinessesProps = {
+    businesses: [
+        business: {
+            id: string,
+            imageSrc: string,
+            name: string,
+            address: string,
+            city: string,
+            state: string,
+            zipCode: string,
+            category: string,
+            rating: number,
+            reviewCount: number,
+        },
+    ],
+};
+
+const BusinessList = ({businesses}: BusinessesProps) => {
+    return (
+        <div className="BusinessList">
+            {businesses.map((business, i) => {
+                return <Business key={business.id} business={business} />
+            })}
+        </div>
+    );
+};
+
+export default BusinessList;
