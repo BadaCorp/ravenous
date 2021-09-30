3f4ba29f456dd28d7063a113329f01a8723b6e2d
Basit Bada <basit.bada@commetix.com>
2021-09-30 01:00:48 -0500

    add CORS message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
@@ -6,11 +6,12 @@ import Yelp from '../../util/Yelp'
 
 const App = () => {
   const [businesses, setBusinesses] = useState([]);
-  const [error, setError] = useState(false); 
+  const [inputError, setInputError] = useState(false);
+  const [corsError, setCorsError] = useState(false); 
 
-  // useEffect(() => {
-  //   searchYelp()
-  // });
+  useEffect(() => {
+    alert('Please go to this link below to temporarily enable CORS before using the app:\nhttps://cors-anywhere.herokuapp.com/corsdemo');
+  }, []);
 
   const searchYelp = (term, location, sortBy) => {
     // setBusinesses([]);
@@ -18,34 +19,49 @@ const App = () => {
       // console.log('business: ', business);
       if(business) {
         setBusinesses(business);
-        setError(false);
+        setInputError(false);
+        setCorsError(false);
       }
       else {
         setBusinesses([]);
-        setError(true);
-        console.err(`Error: Search inputs are Invalid`)
+        setInputError(true);
+        console.log(`Error: Search inputs are Invalid`)
       }
     })
-    .catch(err => console.log('Error: ', err));
-    //console.log('businessses: ', businesses);
+    .catch(err => {
+      setCorsError(true);
+      console.log('Error: ', err)
+    });
   };
 
   return (
     <div className="App">
-      {/* {Yelp.searchYelp('Mexican', 'Seattle', 'best_match').then(buss => console.log(buss.id))} */}
       <h1>ravenous</h1>
       <SearchBar 
         searchYelp={searchYelp}
       />
-      {error? 
+
+      {corsError?
+        <div className='error-msg'>
+          <p className='error-msg-center'>
+            The Yelp api does not support CORS. Please, make sure that you have temporarily enabled
+              &nbsp;<a href='https://cors-anywhere.herokuapp.com/corsdemo'>CORS</a>&nbsp;
+              in your browser inorder to use this app by navigating to this
+              &nbsp;<a href='https://cors-anywhere.herokuapp.com/corsdemo'>link</a> and clicking on the 'Request temporary access to the demo server' button. For more information about CORS, click here:
+              &nbsp;<a href='https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS'>Cross-Origin Resource Sharing (CORS)</a> 
+          </p>
+        </div>: ''
+      }
+
+      {inputError? 
         <div className='error-msg'>
           <p className='error-msg-center'>Inputs invalid!</p><br />
           <p>Please, enter the name of a resturant business (e.g. Pizza, KFC, Mexican etc.) and the name of a city or country (e.g. Lisbon, Toronto, U.S, Cancun etc.)</p>
         </div> : 
         ''
       }
-      <BusinessList businesses={businesses} />
 
+      <BusinessList businesses={businesses} />
     </div>
   );
 };
