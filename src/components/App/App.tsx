62c4c96687de4a7787a5bd656ee0950b19a2f109
Basit Bada <basitbada@Basits-Mac-mini.local>
2024-06-11 04:38:37 -0500

    fix cors errors

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,76 +1,56 @@
-import {useState, useEffect} from 'react';
-import './App.scss';
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
-import Yelp from '../../util/Yelp'
+import { useState, useEffect } from "react";
+import "./App.scss";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+import Yelp from "../../util/Yelp";
 
 export interface BusinessConfig {
-  id: string,
-  imageSrc: string,
-  name: string,
-  address: string,
-  city: string,
-  state: string,
-  zipCode: string,
-  category: string,
-  rating: number,
-  reviewCount: number,
-};
+  id: string;
+  imageSrc: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  category: string;
+  rating: number;
+  reviewCount: number;
+}
 
 export const App = () => {
   const [businesses, setBusinesses] = useState<BusinessConfig[]>([]);
   const [inputError, setInputError] = useState(false);
-  const [corsError, setCorsError] = useState(false); 
 
-  useEffect(() => {
-    alert('Please go to this link below to temporarily enable CORS before using the app:\nhttps://cors-anywhere.herokuapp.com/corsdemo');
-  }, []);
-
-  const searchYelp = (term:string, location:string, sortBy:string) => {
-    Yelp.search(term, location, sortBy).then((business) => {
-      if(business) {
-        setBusinesses(business);
-        setInputError(false);
-        setCorsError(false);
-      }
-      else {
-        setBusinesses([]);
-        setInputError(true);
-        console.log(`Error: Search inputs are Invalid`)
-      }
-    })
-    .catch(err => {
-      setCorsError(true);
-      console.log('Error: ', err)
-    });
+  const searchYelp = (term: string, location: string, sortBy: string) => {
+    Yelp.search(term, location, sortBy)
+      .then((business) => {
+        if (business) {
+          setBusinesses(business);
+          setInputError(false);
+        } else {
+          setBusinesses([]);
+          setInputError(true);
+        }
+      })
+      .catch((err) => {});
   };
 
   return (
     <div className="App">
       <h1>ravenous</h1>
-      <SearchBar 
-        searchYelp={searchYelp}
-      />
+      <SearchBar searchYelp={searchYelp} />
 
-      {corsError?
-        <div className='error-msg'>
-          <p className='error-msg-center'>
-            The Yelp api does not support CORS. Please, make sure that you have temporarily enabled
-              &nbsp;<a href='https://cors-anywhere.herokuapp.com/corsdemo' target='_blank' rel="noreferrer">CORS</a>&nbsp;
-              in your browser inorder to use this app by navigating to this
-              &nbsp;<a href='https://cors-anywhere.herokuapp.com/corsdemo' target='_blank' rel="noreferrer">link</a> and clicking on the 'Request temporary access to the demo server' button. For more information about CORS, click here:
-              &nbsp;<a href='https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS' target='_blank' rel="noreferrer">Cross-Origin Resource Sharing (CORS)</a> 
+      {inputError && (
+        <div className="error-msg">
+          <p className="error-msg-center">Inputs invalid!</p>
+          <br />
+          <p>
+            Please, enter the name of a resturant business (e.g. Pizza, KFC,
+            Mexican etc.) and the name of a city or country (e.g. Lisbon,
+            Toronto, U.S, Cancun etc.)
           </p>
-        </div>: ''
-      }
-
-      {inputError? 
-        <div className='error-msg'>
-          <p className='error-msg-center'>Inputs invalid!</p><br />
-          <p>Please, enter the name of a resturant business (e.g. Pizza, KFC, Mexican etc.) and the name of a city or country (e.g. Lisbon, Toronto, U.S, Cancun etc.)</p>
-        </div> : 
-        ''
-      }
+        </div>
+      )}
 
       <BusinessList businesses={businesses} />
     </div>
--- src/util/Yelp.tsx
+++ src/util/Yelp.tsx
@@ -1,40 +1,37 @@
-const apiKey = 'CPCkBOEMDrCBr1oLQ5RSKh9jEGzmiIOirjlVfitBaSGvnwcHLmmSNQbWb5mLfmYc4YhqSMckgZe89dfKy-VCvtHxl_9n-P2QLSFqhOwvEV6IOVmiQpPcMEXvIbNSYXYx';
-
 interface Business {
-    id: string;
-    image_url: string;
-    name: string;
-    location: Record<string, any>;
-    categories: Record<string, any>[];
-    rating: number;
-    review_count: number;
-};
+  id: string;
+  image_url: string;
+  name: string;
+  location: Record<string, any>;
+  categories: Record<string, any>[];
+  rating: number;
+  review_count: number;
+}
 
 const yelp = {
-    async search(term:string, location:string, sortBy:string) {
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-            headers: {
-                Authorization: `Bearer ${apiKey}`
-            }
-        });
-        const jsonResponse = await response.json();
-        if (jsonResponse.businesses) {
-            return jsonResponse.businesses.map((business: Business) => {
-                return {
-                    'id': business.id,
-                    'imageSrc': business.image_url,
-                    'name': business.name,
-                    'address': business.location.address1,
-                    'city': business.location.city,
-                    'state': business.location.state,
-                    'zipCode': business.location.zip_code,
-                    'category': business.categories[0].title,
-                    'rating': business.rating,
-                    'reviewCount': business.review_count,
-                };
-            });
-        }
+  async search(term: string, location: string, sortBy: string) {
+    const response = await fetch(
+      `https://ravenous-api.onrender.com/yelp?term=${term}&location=${location}&sort_by=${sortBy}`
+    );
+
+    const jsonResponse = await response.json();
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map((business: Business) => {
+        return {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count,
+        };
+      });
     }
+  },
 };
 
 export default yelp;
